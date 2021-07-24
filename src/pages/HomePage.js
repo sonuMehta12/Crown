43b274e03decdfbe38e17426bbecb199781a1f1e
sonu mehta <sonu12022000@gmail.com>
2021-07-24 10:58:31 +0530

    section-13

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../sass/HomeStyle.scss'
+import Directoroy from '../components/directory/Directory'
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directoroy />
+    </div>
+  )
+}
+
+export default HomePage
